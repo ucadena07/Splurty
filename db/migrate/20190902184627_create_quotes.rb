230ee2e49b3aa2cfebfa53598151187930152f89
Ulises Cadena <ucadena07@gmail.com>
2019-09-02 21:55:52 +0000

    changes

--- db/migrate/20190902184627_create_quotes.rb
+++ db/migrate/20190902184627_create_quotes.rb
@@ -2,7 +2,7 @@ class CreateQuotes < ActiveRecord::Migration[5.2]
   def change
     create_table :quotes do |t|
       t.string :saying
-      t.string :autor
+      t.string :author
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2019_09_02_184627) do
 
   create_table "quotes", force: :cascade do |t|
     t.string "saying"
-    t.string "autor"
+    t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
