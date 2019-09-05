bc7561deb68d81af80bf7e8f8c6656ebf1ccdaf0
Ulises Cadena <ucadena07@gmail.com>
2019-09-05 05:21:54 +0000

    picture

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -11,10 +11,14 @@ class QuotesController < ApplicationController
      @quote = Quote.create(quote_params)
   if @quote.invalid?
     flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+
   end
 
     redirect_to root_path
 
+  end
+
+  def about
   end
 
   private
